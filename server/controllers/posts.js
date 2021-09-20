0ea141c77780e5b62cb415bdf0c64424cf00cba4
agentmike007 <manujasan23@gmail.com>
2021-09-20 08:14:55 +0530

    initial commit

---
+++ server/controllers/posts.js
@@ -0,0 +1,37 @@
+import Mongoose from 'mongoose';
+import postMessage from '../models/postMessage.js';
+
+
+export const getPosts = async (req,res) => {
+    try{
+        const postMessages = await postMessage.find();
+        res.status(200).json(postMessages);
+    }
+    catch(error){
+        res.status(404).json({message: error.message});
+    }
+}
+
+export const createPost = async (req,res) => {
+    const post = req.body;
+
+    const newPost = new postMessage(post);
+    try{
+        await newPost.save();
+        res.status(200).json(newPost);
+    }
+    catch(error){
+        res.status(409).json({message:error.message});
+    }
+}
+
+export const updatePost = async(req,res) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+
+    if(!Mongoose.Types.Objects.isValid(_id)) return res.status(404).send('No post with that status')
+
+    const updatedPost = await postMessage.findByIdAndUpdate(_id, post,{new: true});
+
+    res.json(updatedPost);
+}
