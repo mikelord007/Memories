aa03f134b9c971140356da34946238394d80ff3c
agentmike007 <manujasan23@gmail.com>
2021-09-21 10:08:44 +0530

    added client

---
+++ client/src/component/Posts/Posts.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Grid, CircularProgress} from '@material-ui/core';
+import {useSelector} from 'react-redux';
+
+import Post from './Post/post.js';
+import useStyles from './styles.js';
+
+
+const Posts = ({setCurrentId}) =>{
+    const posts = useSelector((state) => state.posts)
+    const classes = useStyles();
+
+    console.log('posts are',posts);
+
+    return(
+        !posts.length? <CircularProgress/> : (
+            <Grid classesName={classes.container} container alignItems="stretch" spacing={3}>
+                {
+                    posts.map((post) => (
+                        <Grid item key={posts.id} xs={12} sm={6}>
+                            <Post post={post} setCurrentId={setCurrentId} />
+                        </Grid>
+                    ))
+                }
+            </Grid>
+        )
+    );
+}
+
+export default Posts;
